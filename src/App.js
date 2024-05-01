59b94ad5509f85d79b2ed02a1d21064893bb72f1
cyber141 <cyberzzz2k3@gmail.com>
2024-05-01 14:09:16 +0700

    func: update template

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import AuthTemplate from "./redux/templates/AuthTemplate";
-import LoginPage from "./pages/loginPage/LoginPage";
+import LoginPage from "./pages/loginPage/loginPage";
 import SignupPage from "./pages/signupPage/SignupPage";
+import HomePage from "./pages/HomePage/HomePage";
+import HomeTemplate from "./templates/HomeTemplate";
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         {/* home template */}
         {/* Add your home route here */}
-        
+        <Route path="" element={<HomeTemplate />}>
+          <Route index element={<HomePage />} />
+        </Route>
+
         {/* auth template */}
         <Route path="/auth" element={<AuthTemplate />}>
           <Route path="login" element={<LoginPage />} />
---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const HomePage = () => {
+  return <div className="z-20" style={{ backgroundColor: "#fdfcf0" }}></div>;
+};
+
+export default HomePage;
